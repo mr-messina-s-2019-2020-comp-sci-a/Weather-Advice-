03a2a7a33663cb36de200c01ae8c3de312ee5481
gharper11 <55795447+gharper11@users.noreply.github.com>
2019-10-07 08:29:08 -0500

    1.1.4 Garrett Final Weather Advice code

---
+++ 1_1_4_Garrett/weather2.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class weather here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class weather2
+{
+    String weatherCondition = "";
+    public weather2(String condition){
+       weatherCondition = condition;
+    }
+    void weather(String x)
+    {   x.substring(0,5);
+        System.out.println(x.substring(0,5));
+    }
+}
